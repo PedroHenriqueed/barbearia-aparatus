9edb0586263ba996a115e849090bfe71ca398adb
PedroHenriqueed <pedrohenrique12davi@gmail.com>
2025-11-12 09:24:28 -0300

    feat: add booking item components

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,46 @@
+import { Card } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { AvatarImage, Avatar } from "./ui/avatar";
+
+interface BookingItemProps {
+ serivceName: string;
+ barbeshopName: string;
+ barbershopImageUrl: string;
+ date: Date;
+}
+
+
+const bookingItem = ({
+    serivceName, barbeshopName, barbershopImageUrl, date}: BookingItemProps) => {
+  return (
+    <Card className="flex flex-row items-center justify-between w-full min-w-full p-0">
+        {/* ESQUERDA */}
+        <div className="flex flex-col gap-4 flex-1 p-4">
+            <Badge>
+                Confirmado
+            </Badge>
+        <div className="flex flex-col gap-2">
+            <p className="font-bold">{serivceName}</p>
+            <div className="flex items-center gap-2 ">
+            <Avatar className="h-6 w-6">
+                       <AvatarImage src={barbershopImageUrl} />
+                </Avatar>
+                <p className="text-sm text-muted-foreground">{barbeshopName}</p>
+            </div>
+        </div>
+        </div>
+        {/* DIREITA */}
+        <div className="flex flex-col items-center justify-center p-4 h-full border-l py-3 min-h-full">
+            <p className="text-xs capitalize">
+                {date.toLocaleDateString('pt-BR', {month: 'long'})}
+            </p>
+            <p>{date.toLocaleDateString ('pt-BR',{day: '2-digit'})}
+            </p>
+            <p className="text-xs capitalize">
+                {date.toLocaleTimeString ('pt-BR',{hour: '2-digit', minute: '2-digit'})}
+            </p>
+        </div>
+    </Card>
+  )
+}   
+export default bookingItem;
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,22 @@ import Header from './_components/header';
 import SearchInput from './_components/search-input';
 import Image from 'next/image';
 import banner from '../public/banner.png';
+import BookingItem from './_components/booking-item';
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Header/>
       <div className="px-5 space-y-4">
       <SearchInput/>
       <Image src={banner} alt="Agende agora" sizes="100vw" className='h-auto w- w-full' />
-    </div>
-    </div>
+      <h2 className='text-xs text-foreground font-semibold uppercase'>Agendamentos</h2>
+      <BookingItem
+      serivceName="Corte de Cabelo" barbeshopName="Barbearia do Zé" barbershopImageUrl="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"
+      date={new Date()}     
+      />
+      </div>
+    </main>
   );
 }
 export default Home;
