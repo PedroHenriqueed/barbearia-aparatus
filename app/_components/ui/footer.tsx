7f85969cb65a6ba4e7f9b8fbe3e973488a31df16
PedroHenriqueed <pedrohenrique12davi@gmail.com>
2025-11-12 11:32:09 -0300

    feat:add footer

---
+++ app/_components/ui/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="p-7 bg-muted">
+        <p className="text-foreground text-xs font-semibold text-center">
+        © 2024 FSW Apparatus. All rights reserved.
+        </p>
+        <p className=" p-2 text-foreground text-xs text-center">
+        Desenvolvido por Pedro Henrique
+        </p>
+    </footer>
+  );
+}      
+export default Footer;
